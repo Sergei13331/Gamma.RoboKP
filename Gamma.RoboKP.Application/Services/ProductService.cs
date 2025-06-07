62611154f898914725234b38712e2779db52ddb0
Sergei Protasov <protas1331@gmail.com>
2025-06-07 15:18:46 +0400

    Добавлены базовые CRUD операции для Product в сервисы

--- Gamma.RoboKP.Application/Services/ProductService.cs
+++ Gamma.RoboKP.Application/Services/ProductService.cs
@@ -10,4 +10,29 @@ public class ProductService(IProductRepository productRepository) : IProductServ
     {
         return await productRepository.Create(product);
     }
+
+    public async Task<ProductEntity?> GetProduct(long id)
+    {
+        return await productRepository.Get(id);
+    }
+
+    public async Task<List<ProductEntity>> GetProducts()
+    {
+        return await productRepository.GetAll();
+    }
+
+    public Task<long> UpdateProductData(long id, string name, string description, decimal price)
+    {
+        return productRepository.Update(id, name, description, price);
+    }
+
+    public Task<long> UpdateProductImage(long id, byte[] image)
+    {
+        return productRepository.UpdateImage(id, image);
+    }
+
+    public Task<bool> RemoveProduct(long id)
+    {
+        return productRepository.Delete(id);
+    }
 }
--- Gamma.RoboKP.Domain/Abstractions/Services/IProductService.cs
+++ Gamma.RoboKP.Domain/Abstractions/Services/IProductService.cs
@@ -5,4 +5,9 @@ namespace Gamma.RoboKP.Domain.Abstractions.Services;
 public interface IProductService
 {
     Task<long> CreateProduct(ProductEntity product);
+    Task<ProductEntity?> GetProduct(long id);
+    Task<List<ProductEntity>> GetProducts();
+    Task<long> UpdateProductData(long id, string name, string description, decimal price);
+    Task<long> UpdateProductImage(long id, byte[] image);
+    Task<bool> RemoveProduct(long id);
 }
